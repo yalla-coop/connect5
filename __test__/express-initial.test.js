ac30538c1c15d6b2abeb28344ef3ca32cbc1ea4d
Joe Friel <joseph.s.friel@gmail.com>
2018-11-13 16:57:22 +0000

    Add initial testing > relates #44

---
+++ __test__/express-initial.test.js
@@ -0,0 +1,3 @@
+test("basic maths to show jest works: 11 + 22 === 33", () => {
+  expect(11 + 22).toBe(33);
+});
--- server/app.js
+++ server/app.js
@@ -1,21 +1,30 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const controllers = require('./controllers');
+const controllers = require("./controllers");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.use(controllers);
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry - can't find that!");
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(`Something broke! ${err}`);
 });
 
 module.exports = app;
