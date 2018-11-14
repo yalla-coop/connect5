f6f29035a6fa22f07078cad07bfb4feebd1bffb8
Simon Dupree <dupreenator@gmail.com>
2018-11-14 12:10:32 +0000

    Trainers model setup > relates#54

---
+++ server/models/Trainer.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+
+const TrainerSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = Trainer = mongoose.model('trainers', TrainerSchema);
