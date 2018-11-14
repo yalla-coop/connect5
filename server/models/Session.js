8fdc6794b856140454184821f8a227b6782b0e50
Simon Dupree <dupreenator@gmail.com>
2018-11-14 12:17:20 +0000

    Sessions model setup > relates#54

---
+++ server/models/Session.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+
+const SessionSchema = new Schema({
+  trainer: {
+    // connect each session to a trainer
+    type: Schema.Types.ObjectId, //FK ref trainer_id
+    ref: 'trainers'
+  }
+  type: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  invitees: {
+    type: Number,
+  },
+  attendees: {
+    type: Number,
+  },
+  surveyURL1: {
+    type: String,
+    required: true
+  },
+  surveyURL2: {
+    type: String,
+  }
+});
+
+module.exports = Session = mongoose.model('sessions', SessionSchema);
