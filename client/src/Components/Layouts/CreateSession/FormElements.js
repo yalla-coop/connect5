1f992c86ba2e353cbb60de636e20d7880d29a290
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-11-16 00:01:30 +0200

    put options & styled components in seperated files

---
+++ client/src/Components/Layouts/CreateSession/FormElements.js
@@ -0,0 +1,86 @@
+
+import Select from "react-select";
+import styled from "styled-components";
+import DatePicker from "react-datepicker";
+
+const Contianer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 25px;
+`;
+const Heading = styled.h1`
+  color: var( --heading-color);
+  font-size: 28px;
+  text-align: center;
+  font-weight: 900
+`;
+
+const Form = styled.form`
+  background:none;
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  margin: 0 auto;
+  padding: 15px 14px;
+  background-color: #ECECEC;
+  border-radius: 15px;
+  border: 1px solid var(--border-color)
+`;
+
+const Input = styled.input`
+  height: 50px;
+  border-radius: 4px;
+  outline: none;
+  margin: 13px 0;
+  border: 1px solid var(--border-color);
+  padding-left: 25px;
+`;
+
+const Date = styled(DatePicker)`
+  height: 50px;
+  border-radius: 4px;
+  outline: none;
+  margin: 13px 0;
+  border: 1px solid var(--border-color);
+  padding-left: 25px;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  line-height: 50px;
+  margin: 15px 0;
+  border-radius: 5px;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 1.4em
+  color: black;
+  text-decoration: none;
+  background-color: var(--button-background-color);
+  color: var(--button-text-color);
+  outline: none;
+  border: none;
+`;
+const SelectComponent = styled(Select)`
+  > :first-child{
+    height: 50px;
+    border-radius: 5px;
+    border: 1px solid var(--border-color);
+  }
+`;
+
+const Error = styled.p`
+  color: red;
+  padding: 0;
+  margin: 20px auto 0;
+`;
+
+export {
+  Contianer,
+  Heading,
+  Form,
+  Input,
+  Button,
+  SelectComponent,
+  Date,
+  Error,
+};
--- client/src/Components/Layouts/CreateSession/index.js
+++ client/src/Components/Layouts/CreateSession/index.js
@@ -1,40 +1,63 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import DatePicker from "react-datepicker";
-import moment from "moment";
+
+import {
+  Button,
+  Form,
+  Contianer,
+  Input,
+  SelectComponent,
+  Date,
+  Heading,
+  Error,
+} from "./FormElements";
+import options from "./options";
+
 import "react-datepicker/dist/react-datepicker.css";
-
-
-const Contianer = styled.div`
-
-`;
-const Heading = styled.h1`
-
-`;
-
-const Form = styled.form`
-  background:none;
-  color: red
-`;
-
-const Input = styled.input`
-  
-`;
-
 
 class CreateSession extends Component {
   state = {
-    startDate: moment(),
+    session: "",
+    startDate: null,
+    attendantsNumber: "",
+    err: false,
   };
 
-  handleChange=(date) => {
+  handleDateChange = (date) => {
     this.setState({
       startDate: date,
     });
   }
 
+  handleInputChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSelectChange = (session) => {
+    this.setState({
+      session,
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { startDate, attendantsNumber, session } = this.state;
+    if (!!startDate && !!attendantsNumber && !!session) {
+      this.setState({
+        err: false,
+      });
+    } else {
+      this.setState({
+        err: true,
+      });
+    }
+  }
+
   render() {
-    const { startDate } = this.state;
+    const {
+      startDate, attendantsNumber, session, err,
+    } = this.state;
 
     return (
       <Contianer>
@@ -42,14 +65,36 @@ class CreateSession extends Component {
         <Heading>
         Create New Session
         </Heading>
-        <DatePicker
-          selected={startDate}
-          onChange={this.handleChange}
-        />
-        <Form>
-          <Input type="number" />
-          <Input type="number" />
+
+        <Form onSubmit={e => this.handleSubmit(e)}>
+
+          <Date
+            selected={startDate}
+            placeholderText="Click to select a date"
+            onChange={this.handleDateChange}
+            name="startDate"
+            dateFormat="YYYY-MM-DD"
+          />
+
+          <Input
+            type="number"
+            placeholder="Number of session attendants"
+            value={attendantsNumber}
+            onChange={this.handleInputChange}
+            name="attendantsNumber"
+          />
+
+          <SelectComponent
+            options={options}
+            onChange={this.handleSelectChange}
+            placeholder="Click to selesct session No."
+            selected={session}
+          />
+          { err && <Error>All inputs are required</Error> }
+          <Button type="submit">Submit</Button>
+
         </Form>
+
       </Contianer>
     );
   }
