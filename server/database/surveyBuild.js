c39b78b8797d91063412898e0ddf7e9bf22b9d1c
Joe Friel <joseph.s.friel@gmail.com>
2018-11-15 11:17:01 +0000

    Add presurvey question data for build > rel issue #55

--- server/database/surveyBuild.js
+++ server/database/surveyBuild.js
@@ -23,15 +23,81 @@ Question.deleteMany({})
       // Pre-survey questions
       {
         surveyType: 0,
-        questionText: "Please provide the name of your trainer",
-        inputType: "textarea"
+        questionText: "Please enter your Unique ID Number (if applicable)",
+        inputType: "text"
       },
       {
         surveyType: 0,
         questionText: "Please select the region",
         inputType: "radio",
-        helperText: "",
-        options: ["North East", "North West", "London"]
+        options: [
+          "North East",
+          "North West",
+          "Yorkshire and the Humber",
+          "East Midlands",
+          "West Midlands",
+          "East of England",
+          "London",
+          "South East",
+          "South West",
+          "Other (please specify)"
+        ]
+      },
+      {
+        surveyType: 0,
+        questionText: "Please enter the postcode where you are active",
+        inputType: "text",
+        helperText:
+          "If you do not work in a fixed location please put the postcode your are most often working in",
+        options: []
+      },
+      {
+        surveyType: 0,
+        questionText: "Please enter your job title",
+        inputType: "text"
+      },
+      {
+        surveyType: 0,
+        questionText: "Please select your workforce",
+        inputType: "radio",
+        options: [
+          "Emergency services (including fire service, police, ambulance)",
+          "Public health specialists and practitioners (e.g. public health consultants, health improvement managers, smoking cessation advisors)",
+          "Welfare (e.g. employment advisers, benefits case workers, advisers working on a voluntary basis)",
+          "Community health promotion workers/volunteers (e.g. health trainers, health champions, health and wellbeing advisors, breastfeeding volunteers)",
+          "Health Professionals (e.g. GPs, nurses, Allied Health Professionals)",
+          "Social care and housing professionals (housing officers, social workers, youth workers and other social care professions)",
+          "Teaching and educational professionals (e.g. headteachers, teachers, teaching assistants, admin staff working in education settings)",
+          "Childcare related professions (e.g. nursery staff, childminders)",
+          "Sports and fitness occupations (e.g. sports coaches, fitness instructors and leisure centre employees)",
+          "Other (please specify)"
+        ]
+      },
+      {
+        surveyType: 0,
+        questionText:
+          "How would you rate your current knowledge and understanding of mental health and wellbeing?",
+        inputType: "radiostar",
+        helperText:
+          "0 stars means no understanding. 6 stars means excellent understanding",
+        options: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        surveyType: 0,
+        questionText:
+          "In your role, how important is it for you to support somebody with their mental health and wellbeing?",
+        inputType: "radiostar",
+        helperText:
+          "0 stars means not important. 6 stars means every important",
+        options: [1, 2, 3, 4, 5, 6]
+      },
+      {
+        surveyType: 0,
+        questionText:
+          "How confident do you feel in having conversations with individuals about mental health and wellbeing?",
+        inputType: "radiostar",
+        helperText: "0 stars means not confident. 6 stars means very confident",
+        options: [1, 2, 3, 4, 5, 6]
       }
 
       // Survey 1 questions
