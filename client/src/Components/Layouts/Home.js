15bcec73cd16140d6f2b9f327df27c34f1f04bd1
Joe Friel <joseph.s.friel@gmail.com>
2018-11-16 12:41:52 +0000

    Fix navbar test > rel issue #61

--- client/src/Components/Layouts/Home.js
+++ client/src/Components/Layouts/Home.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 
-import Navbar from "../CommonComponents/Navbar/index";
-
 class Home extends Component {
   render() {
     return (
       <React.Fragment>
         <div className="home">Home</div>
-        <Navbar />
       </React.Fragment>
     );
   }
