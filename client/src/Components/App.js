af9bf2d341ea7f8d194a44f17a590189fd9be203
Joe Friel <joseph.s.friel@gmail.com>
2018-11-16 12:57:22 +0000

    Remove unused navbar variable > rel issue #61

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./CommonComponents/Navbar";
 import Home from "./Layouts/Home";
 
 import "./App.css";
