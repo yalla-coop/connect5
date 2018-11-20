fa36a18c35472c35b1e65fc2acf717c2c63783af
Simon Dupree <dupreenator@gmail.com>
2018-11-20 11:05:38 +0000

    unused react components deleted for login and register

--- client/src/Components/auth/Register.js
+++ client/src/Components/auth/Register.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Header from "../CommonComponents/Header";
-
 import {
   Heading, Container, Form, Input, Error,
 } from "./PageElementsAuth";
