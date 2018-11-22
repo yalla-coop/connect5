2cca8bc058ef22a46449d7ddf0bf7d32485b5bb2
Marwa Jomaa <34250163+marwajomaa@users.noreply.github.com>
2018-11-23 01:30:00 +0200

    Update Link.js

--- client/src/Components/Layouts/Session-Details/Link.js
+++ client/src/Components/Layouts/Session-Details/Link.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
   Span2, Span1, LinkType, LinkInfo, Btn,
 } from "./styledComponents";
