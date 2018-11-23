d55686c9f1060c6c1aac136f2f52221da3c52d51
Simon Dupree <dupreenator@gmail.com>
2018-11-23 16:30:20 +0000

    set up individual response component rendering answers related to inputType

--- client/src/Components/Layouts/SessionResult/IndividualResults/Answer.js
+++ client/src/Components/Layouts/SessionResult/IndividualResults/Answer.js
@@ -3,43 +3,87 @@ import PropTypes from "prop-types";
 
 import { IndividualAnswer, QuestionSpan, MatrixAnswer } from "../StyledComponents";
 
-const Answer = ({ answer }) => (
-  <div>
-    {
-      answer.inputType === "matrix" || answer.inputType === "checkbox"
-        ? (
+class Answer extends React.Component {
+  render() {
+    const { answer } = this.props;
+    if (answer.inputType === "checkbox") {
+      return (
+        <React.Fragment>
           <ul>
-            {answer && answer.answer.map(item => (
-              <li key={Math.random()}>
-
-                <MatrixAnswer>
-                  {item}
-                </MatrixAnswer>
-              </li>
-            ))}
+            {answer
+              && answer.answer.map(item => (
+                <li key={Math.random()}>
+                  <MatrixAnswer>{item}</MatrixAnswer>
+                </li>
+              ))}
           </ul>
-
-        )
-        : (
-          <IndividualAnswer>
-            <QuestionSpan>
-                  A.
-            </QuestionSpan>
-            {answer.answer}
-            {
-              answer.inputType === "radiostar"
-                && (
-                  <span>
-                    {" "}
-                          stars (out of 6)
-                  </span>
-                )
-            }
-          </IndividualAnswer>
-        )
+        </React.Fragment>
+      );
     }
-  </div>
-);
+    if (answer.inputType === "matrix") {
+      const answerArray = Object.entries(answer.answer);
+      return (
+        <React.Fragment>
+          {answer
+            && answerArray.map(item => (
+              <div key={Math.random()}>
+                <MatrixAnswer>
+                  <h4>{item[0]}</h4>
+                  <p>{item[1]}</p>
+                </MatrixAnswer>
+              </div>
+            ))}
+        </React.Fragment>
+      );
+    }
+    return (
+      <IndividualAnswer>
+        <QuestionSpan>A.</QuestionSpan>
+        {answer.answer}
+        {answer.inputType === "radiostar" && <span> stars (out of 6)</span>}
+      </IndividualAnswer>
+    );
+  }
+}
+
+// const Answer = ({ answer }) => (
+
+// <div>
+//   {
+//     answer.inputType === "matrix" || answer.inputType === "checkbox"
+//       ? (
+//         <ul>
+//           {answer && answer.answer.map(item => (
+//             <li key={Math.random()}>
+
+//               <MatrixAnswer>
+//                 {item}
+//               </MatrixAnswer>
+//             </li>
+//           ))}
+//         </ul>
+
+//       )
+//       : (
+//         <IndividualAnswer>
+//           <QuestionSpan>
+//                 A.
+//           </QuestionSpan>
+//           {answer.answer}
+//           {
+//             answer.inputType === "radiostar"
+//               && (
+//                 <span>
+//                   {" "}
+//                         stars (out of 6)
+//                 </span>
+//               )
+//           }
+//         </IndividualAnswer>
+//       )
+//   }
+// </div>
+// );
 
 export default Answer;
 
