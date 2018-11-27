90b91b96a8d14d360c5446604844ff4c9f21e964
Simon Dupree <dupreenator@gmail.com>
2018-11-27 16:47:50 +0000

    logic unanswered question ids on form submit to be put into error obj > relates #69

--- server/validation/survey-answers-val.js
+++ server/validation/survey-answers-val.js
@@ -7,15 +7,22 @@ const validateSurveyInput = async (data) => {
   const errors = {};
 
   // query the Question model using data.surveyType to get a list of the question ids for that survey which are required
-  const questions = await Questions.find({ surveyType: 1, isRequired: true });
+  const questions = await Questions.find({ surveyType: data.surveyType, isRequired: true });
+  // create array of question ids required
+  const questionIDList = questions.map(e => e._id.toString());
+  // create array of question ids for answers submitted
+  const answers = Object.keys(data.formState);
+  // filter questionIdArray to find unanswered questions and put them into error obj
+  questionIDList.filter((e) => {
+    if (answers.includes(e)) {
+      errors[e] = "this question must be answered";
+    }
+  });
 
   console.log("Qs", questions);
-
-  // loop through our answers and match against the question ids from our Question model
-
-  // if any are missing, add them to the errors object
-
-  // loop through the answer object
+  console.log("answers", answers);
+  console.log("questionIDS", questionIDList);
+  console.log("errors", errors);
 };
 
 module.exports = validateSurveyInput;
