6383828ed72b36020a3527f0305bc75cd1cdfa69
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-11-28 11:54:26 +0200

    add index file for OverviewResults

--- client/src/Components/Layouts/OverviewResults/index.js
+++ client/src/Components/Layouts/OverviewResults/index.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import {
-  ResultsOverviewWrapper, PageTitle, StatisicsContainer, TabsWrapper, Tab,
+  ResultsOverviewWrapper, PageTitle, StatisicsContainer, TabsWrapper, Tab, SmallTitle, QuestionWrapper, QuestionText, Triangle,
 } from "./StyledComponents";
 
+import AttendanceResults from "./AttendanceResults";
+import ResponsesResults from "./ResponsesResults";
+import Popup from "./Popup";
+
+
 class ResultsOverview extends Component {
-state = {
-  activeTab: "attendance",
-}
+  state = {
+    activeTab: "attendance",
+    isPopupActive: false,
+    activeQuestionIndex: 0,
+    attendees: [],
+    responses: [],
+    radiostarQuestions: [],
+  }
+
+  componentDidMount() {
+    const { history, match } = this.props;
+
+    axios.get("/trainer/overview")
+      .then(({ data }) => data.map(item => this.setState(item)))
+      .then(() => {
+        axios.get("/question/radiostart/all")
+          .then(({ data }) => {
+            this.setState({ radiostarQuestions: data });
+            console.log(data, "radiostarQuestions");
+          });
+      })
+      .catch(() => history.push("/server-error"));
+  }
 
   handleTabClick = (e) => {
     this.setState({
@@ -15,9 +41,26 @@ state = {
     });
   }
 
+  handleClosePopup = () => {
+    this.setState({
+      isPopupActive: false,
+    });
+  }
+
+
+  handleOpenPopup = (event) => {
+    window.scrollTo(0, 0);
+    event.preventDefault();
+    const id = event.target.id || event.target.parentNode.id;
+    this.setState({
+      isPopupActive: true,
+      activeQuestionIndex: id,
+    });
+  }
+
   render() {
-    const { activeTab } = this.state;
-    const { handleTabClick } = this;
+    const { activeTab, isPopupActive, activeQuestionIndex } = this.state;
+    const { handleTabClick, handleOpenPopup, handleClosePopup } = this;
 
     return (
       <ResultsOverviewWrapper>
@@ -42,16 +85,46 @@ state = {
             </Tab>
           </TabsWrapper>
           {
-            activeTab === "overall"
-              ? <AttendanceResults />
-              : <ResponsesResults />
+            activeTab === "attendance"
+              ? <AttendanceResults attendees={this.state.attendees} />
+              : <ResponsesResults responses={this.state.responses} />
           }
 
         </StatisicsContainer>
+        <SmallTitle>
+            Responses
+        </SmallTitle>
+        {
+          this.state.radiostarQuestions.map((item, index) => (
+            <QuestionWrapper onClick={handleOpenPopup} id={index}>
+              <QuestionText>
+                {item._id}
+              </QuestionText>
+              <Triangle />
+            </QuestionWrapper>
+          ))
+        }
 
+        {isPopupActive
+          ? (
+            <Popup
+              question={this.state.radiostarQuestions[activeQuestionIndex]}
+              handleClosePopup={handleClosePopup}
+            />
+          )
+          : null}
       </ResultsOverviewWrapper>
     );
   }
 }
 
 export default ResultsOverview;
+/* <Popup
+handleClosePopup={handleClosePopup}
+questionId={activeQuestion._id}
+inputType={activeQuestion.inputType}
+questionText={activeQuestion.questionText}
+question={activeQuestion}
+sessionId={match.params.sessionId}
+history={history}
+/> */
