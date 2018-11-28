ec1660c395319c8d6f044e693c4b1521c6589e5e
Michael Watts <hello@michaelwatts.co>
2018-11-28 07:51:54 +0000

    add data to axios mock

--- client/__mocks__/axios.js
+++ client/__mocks__/axios.js
@@ -1,4 +1,31 @@
+const viewSessions = [
+  {
+    __v: 0,
+    _id: "5bf3cd830032112611baa750",
+    attendees: 8,
+    date: "2018-04-17T00:00:00.000Z",
+    invitees: 15,
+    surveyURL1: "connect5.com/presurvey",
+    surveyURL2: "connect5.com/survey1",
+    trainer: "5bf3cd830032112611baa74f",
+    type: 1,
+  },
+  {
+    __v: 0,
+    _id: "5bf3cd830032112611baa751",
+    date: "2018-08-22T00:00:00.000Z",
+    invitees: 6,
+    surveyURL1: "connect5.com/survey2",
+    trainer: "5bf3cd830032112611baa74f",
+    type: 2,
+  },
+];
+
+const routeData = {
+  "/view-sessions": viewSessions,
+};
+
 export default {
-  get: jest.fn(Promise.resolve({ data: 123 })),
-  post: jest.fn(Promise.resolve({ data: 123 })),
+  get: jest.fn(url => Promise.resolve({ data: routeData[url] })),
+  post: jest.fn(url => Promise.resolve({ data: routeData[url] })),
 };
