5c974100614814798809b467928a0d5876fb1db9
Simon Dupree <dupreenator@gmail.com>
2018-11-28 15:50:49 +0000

    renamend Logo.jpg to logo.jpg and updated snapshots

--- client/src/Components/CommonComponents/Header/index.js
+++ client/src/Components/CommonComponents/Header/index.js
@@ -2,39 +2,39 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import Logo from "./Logo.jpg";
+import Logo from "./logo.jpg";
 
 const Container = styled.div`
   position: fixed;
   top: 0;
   right: 0;
-  left:0;
+  left: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 55px;
-  box-sizing:border-box;
+  box-sizing: border-box;
   padding: 0 20px;
 
-    + * {
-      margin-top:55px
-    }
+  + * {
+    margin-top: 55px;
+  }
 
-    :after{
-      content: "";
-      display: block;
-      width:100%;
-      background-color: var( --line-color);
-      position: absolute;
-      top: 55px;
-      height: 3px;
-      left: 0;
-    }
+  :after {
+    content: "";
+    display: block;
+    width: 100%;
+    background-color: var(--line-color);
+    position: absolute;
+    top: 55px;
+    height: 3px;
+    left: 0;
+  }
 `;
 
 const I = styled.i`
   font-size: 35px;
-  color: var(--paragraph-color)
+  color: var(--paragraph-color);
 `;
 
 const Image = styled.img`
