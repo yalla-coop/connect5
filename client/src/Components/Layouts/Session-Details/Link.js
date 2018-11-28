d10fcb26753c4151b527e69a298d0dc7fe53b118
Simon Dupree <dupreenator@gmail.com>
2018-11-28 13:17:54 +0000

    info modal added >relates#91

--- client/src/Components/Layouts/Session-Details/Link.js
+++ client/src/Components/Layouts/Session-Details/Link.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import swal from "sweetalert";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import {
   Span2, Span1, LinkType, SurveyType, LinkInfo, Btn, copyLink,
 } from "./styledComponents";
 
 class Link extends Component {
+  handleInfo = () => {
+    swal({
+      icon: "info",
+      text:
+        "Please copy and send the survey link to all participants. You will see the survey results for your session as soon as they are being submitted",
+    });
+  };
+
   render() {
     const {
       type, onCopy, saveInState, surveyURL1, value,
@@ -15,7 +24,7 @@ class Link extends Component {
           <SurveyType>{type}</SurveyType>
           Link
         </Span1>
-        <Span2>
+        <Span2 onClick={this.handleInfo} style={{ cursor: "pointer" }}>
           <i className="fas fa-info-circle" />
           <LinkInfo>info</LinkInfo>
         </Span2>
