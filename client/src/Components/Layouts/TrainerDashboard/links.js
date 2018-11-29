a4d275c0ca322889153c9208727ff542134b719c
marwa <marwajomaa4@gmail.com>
2018-11-29 20:48:40 +0200

    add dashboard links

---
+++ client/src/Components/Layouts/TrainerDashboard/links.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import {
+  Links, LinkWrapper, IconDiv, Navlink, Img, Span,
+} from "./styledComponents";
+
+
+class DashboardLinks extends Component {
+  render() {
+    return (
+      <Links>
+        <LinkWrapper>
+          <Navlink to="/view-sessions">
+            <IconDiv>
+              <Img src="../Imgs/sessions.jpg" alt="" />
+            </IconDiv>
+            <div>
+              <Span>Sessions</Span>
+            </div>
+          </Navlink>
+        </LinkWrapper>
+
+        <LinkWrapper>
+          <Navlink to="/overview-results">
+            <IconDiv>
+              <Img src="../Imgs/results.jpg" alt="" />
+            </IconDiv>
+            <div>
+              <Span>Results</Span>
+            </div>
+          </Navlink>
+        </LinkWrapper>
+
+        <LinkWrapper>
+          <Navlink to="/create-session">
+            <IconDiv>
+              <Img src="../Imgs/add.jpg" alt="" />
+            </IconDiv>
+            <div>
+              <Span>New Session</Span>
+            </div>
+          </Navlink>
+        </LinkWrapper>
+      </Links>
+    );
+  }
+}
+
+export default DashboardLinks;
