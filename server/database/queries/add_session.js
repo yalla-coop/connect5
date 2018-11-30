3462f099e2cc41132ec48e5239277cb968dd9f47
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-01 00:45:15 +0200

    edit addSession query to take trainerId as argument

--- server/database/queries/add_session.js
+++ server/database/queries/add_session.js
@@ -1,25 +1,22 @@
 const Session = require("./../models/Session");
-const Trainer = require("./../models/Trainer");
 
-const addSession = (sessionType, startDate, attendantsNumber) => new Promise((resolve, reject) => {
-  // Get the default trainer data
-  Trainer.findOne({
-    email: "johndoe@gmail.com",
-  }, "_id")
-    .then((defaultTrainer) => {
-      const session = new Session({
-        trainer: defaultTrainer._id,
-        type: sessionType,
-        date: startDate,
-        invitees: 0,
-        attendees: attendantsNumber,
-        surveyURL1: "null",
-      });
-      // Add new session
-      session.save()
-        .then(resolve)
-        .catch(err => reject(err));
-    })
+const addSession = (
+  trainerId,
+  sessionType,
+  startDate,
+  attendantsNumber,
+) => new Promise((resolve, reject) => {
+  const session = new Session({
+    trainer: trainerId,
+    type: sessionType,
+    date: startDate,
+    invitees: 0,
+    attendees: attendantsNumber,
+    surveyURL1: "null",
+  });
+  // Add new session
+  session.save()
+    .then(resolve)
     .catch(err => reject(err));
 });
 
