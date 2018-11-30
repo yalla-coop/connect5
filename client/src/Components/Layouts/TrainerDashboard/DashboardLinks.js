8769c79b03c8acfec0382251f2ddbd5fb30548c1
Joe Friel <joseph.s.friel@gmail.com>
2018-11-30 12:27:31 +0000

    Delete unused components

--- client/src/Components/Layouts/TrainerDashboard/DashboardLinks.js
+++ client/src/Components/Layouts/TrainerDashboard/DashboardLinks.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios";
-import setAuthToken from "../../../Utils/setAuthToken";
 import {
-  Statistics,
   Links,
   LinkWrapper,
   IconDiv,
