5ce1bc7c49118ffec5b0d2e56c4d4af432383cb9
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-02 00:42:11 +0200

    remove unused variable

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 
 // setup authorization
 import jwt_decode from "jwt-decode";
