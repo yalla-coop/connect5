f27d2350e53edabd29e4f1bab89f78a7b4696ce2
Joe Friel <joseph.s.friel@gmail.com>
2018-12-02 09:22:45 +0000

    Remove unused components

--- client/src/Components/Layouts/SessionResult/OverallResults/Matrix.js
+++ client/src/Components/Layouts/SessionResult/OverallResults/Matrix.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { MatrixAnswer, StarsRow } from "../StyledComponents";
+import { StarsRow } from "../StyledComponents";
 
 import getOptions from "./get_options"
 import findRate from "./find_rate"
--- client/src/Components/Layouts/Survey.js
+++ client/src/Components/Layouts/Survey.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import axios from "axios";
 import swal from "sweetalert";
 
