b815ba7ae2d4b7759a36980a8d4818e0dac9dd9f
Joe Friel <joseph.s.friel@gmail.com>
2018-12-02 09:08:20 +0000

    Starting to fix matrix results display

--- client/src/Components/Layouts/SessionResult/OverallResults/Matrix.js
+++ client/src/Components/Layouts/SessionResult/OverallResults/Matrix.js
@@ -1,30 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { MatrixAnswer } from "../StyledComponents";
+import { MatrixAnswer, StarsRow } from "../StyledComponents";
 
-const Matrix = ({ answers }) => (
-  <div>
-    <ul>
-      {
-        answers && answers[0].answer.map(item => (
-          <li key={Math.random()}>
-            <MatrixAnswer>
-              {item}
-            </MatrixAnswer>
-          </li>
-        ))
-      }
-    </ul>
-  </div>
-);
+import getOptions from "./get_options"
+import findRate from "./find_rate"
+
+class Matrix extends Component {
+
+  static propTypes = {
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      answer: PropTypes.arrayOf(PropTypes.string),
+    })),
+  };
+
+  static defaultProps = {
+    answers: [],
+    options: [],
+  };
+
+  state = {
+    options: [],
+  }
+  
+  componentDidUpdate(prevProps) {
+    const { answers, options: rawOptions } = this.props;
+    if (prevProps.answers !== answers) {
+      const { options } = getOptions(answers, rawOptions.columns, true);
+      this.setState({
+        options,
+      });
+    }
+  }
+
+  
+  // const questionArray = Object.keys(answers[0].answer);
+  // console.log("Questions", questionArray)
+  
+  render() {
+
+    const { answers, options: rawOptions } = this.props;
+    const subQuestions = rawOptions.rows;
+    const { options } = this.state;
+
+    const rate = answers && findRate(answers);
+    console.log("MATRIX", this.props)
+    console.log("STATE OPTS", this.state.options)
+    console.log("RATE", rate)
+    
+    
+    return (
+    
+    <React.Fragment>
+      {rawOptions && subQuestions.map(question => (
+        <div key={Math.random()}>
+          <h4>{question}</h4>
+          {options && options.map(option => (
+            <StarsRow key={Math.random()}>
+              <p>{option.value}</p>
+            </StarsRow>
+          ))}
+        </div>
+      ))}
+      
+    </React.Fragment>
+  )};
+  }
 
 export default Matrix;
 
-Matrix.propTypes = {
-  answers: PropTypes.arrayOf(PropTypes.shape({
-    answer: PropTypes.arrayOf(PropTypes.string),
-  })),
-};
-Matrix.defaultProps = {
-  answers: [{}],
-};
