1e07d7eb0299143383614d7417e1785c373aa9bb
Ramyalshurafa <ramyshurafa@hotmail.com>
2018-12-04 22:54:23 +0200

    modify the style for TrainersLandingPage

--- client/src/Components/Layouts/TrainersLandingPage/PageElements.js
+++ client/src/Components/Layouts/TrainersLandingPage/PageElements.js
@@ -8,13 +8,14 @@ const Container = styled.div`
   margin: 0 auto;
   text-align: center;
   height: 100%;
-  margin-top: 120px
+  margin-top: 120px;
+  max-width: 600px
 `;
 
 const Heading = styled.h1`
   font-size: 1.7em;
   margin: 0 auto;
-  color: var(--heading-color)
+  color: var(--main-heading)
 `;
 
 const Paragraph = styled.p`
@@ -23,13 +24,13 @@ const Paragraph = styled.p`
   margin-top: 50px;
   font-size: 0.85em
   text-align: justify;
-  color: var(--paragraph-color);
+  color: var(--button-text-color);
   margin-bottom: 40px
 `;
 
 const Button = styled(Link)`
   background: none;
-  line-height: 35px;
+  line-height: 45px;
   margin: 10px 0;
   border-radius: 5px;
   text-transform: uppercase;
@@ -37,8 +38,8 @@ const Button = styled(Link)`
   font-size: 1.4em
   color: black;
   text-decoration: none;
-  background-color: var(--button-background-color);
-  color: var(--button-text-color);
+  background-color: var(--large-button-background);
+  color: var(--large-button-text);
 `;
 
 export {
