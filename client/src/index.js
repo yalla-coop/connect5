5e23d8bca33868621e16afd3fb39cd93273862f0
marwajomaa <marwajomaa4@gmail.com>
2019-05-12 20:57:51 +0300

    wrapping app component ith provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./Components/App";
+import store from "./redux/store";
 
 import "./index.css";
 import "./normalize.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"),
+);
