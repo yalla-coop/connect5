d1771beded2e396617bdaf19fbd95fd4a84b660b
ramy <ramyshurafa@hotmail.com>
2019-05-14 14:16:59 +0300

    test: response schema

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   rules: {
     "no-unused-vars": ["error", { "args": "none" }],
+    "no-underscore-dangle": [0],
      // configure the prettier plugin
     "prettier/prettier": [
       "error",
