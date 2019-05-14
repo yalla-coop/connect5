a90a73424fa34d0a3c6af5ced938d53fa7c84f6d
Joe Friel <joseph.s.friel@gmail.com>
2019-05-14 14:46:46 +0100

    Remove console warnings

--- v2/client/src/components/common/Toggle/index.js
+++ v2/client/src/components/common/Toggle/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-import { colors, borders, shadows } from "../../../theme";
+import { colors, borders } from "../../../theme";
 
 
 const ToggleWrapper = styled.div`
