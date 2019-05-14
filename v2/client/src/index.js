ce519ab64b6bc9c35ae16384731e780522234abf
Joe Friel <joseph.s.friel@gmail.com>
2019-05-14 12:27:49 +0100

    Set up theme file > #223

--- v2/client/src/index.js
+++ v2/client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 import { Provider } from 'react-redux';
 
 import App from './components/App';
---
+++ v2/client/src/theme.js
@@ -0,0 +1,61 @@
+// media queries
+// so far we only decided to use 1 breakpoint, 768px (tablet)
+export const size = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  mobileXL: '680px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
+};
+
+export const breakpoints = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  mobileXL: `(min-width: ${size.mobileXL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
+  desktopL: `(min-width: ${size.desktop})`,
+};
+
+const colorCodes = {
+  // primary (purple)
+  primary: '#2C3192',
+  lightPrimary: '#787BB9',
+  extralightPrimary: '#C5C6E1',
+
+  // positive, negative and confirm/cancel
+  red: '#E97DA0',
+  green: '#9FCE67',
+
+  // general
+  white: '#FFFFFF',
+  offWhite: '#F7F7F9',
+  gray: '#D4D4D8',
+  lightGray: '#EFEFF4',
+  black: '#4F4F4F',
+};
+
+// general colors
+export const colors = {
+  ...colorCodes,
+  headingUnderline: colorCodes.lightGray,
+  cardBackground: colorCodes.white,
+  profileFontColor: colorCodes.black,
+  links: colorCodes.primary,
+};
+
+// shadows
+export const shadows = {
+  primary: '0px 1px 4px rgba(0, 0, 0, 0.05)',
+};
+
+// borders
+export const borders = {
+  inputBox: `1px solid ${colors.lightGray}`,
+};
