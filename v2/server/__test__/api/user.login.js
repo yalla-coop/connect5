feb68826e2a1521309abe259c98db652f2ab008b
marwajomaa <marwajomaa4@gmail.com>
2019-05-16 15:59:24 +0300

    add api test for login

---
+++ v2/server/__test__/api/user.login.js
@@ -0,0 +1,78 @@
+const request = require('supertest');
+const mongoose = require('mongoose');
+
+const buildDB = require('./../../database/data/development/index');
+const app = require('./../../app');
+
+describe('Tesing for login route', () => {
+  beforeAll(async () => {
+    // build dummy data
+    await buildDB();
+  });
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+  });
+
+  beforeEach(async () => {
+    // build dummy data
+    await buildDB();
+  });
+
+  test('test with correct email and password', done => {
+    const data = {
+      email: 'nisha.sharma@phe.gov.uk',
+      password: '123456',
+    };
+
+    request(app)
+      .post('/api/login')
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        expect(res).toBeDefined();
+        expect(res.body).toBeDefined();
+
+        expect(res.body.id).toBeDefined();
+        done();
+      });
+  });
+
+  test('test with invalid request email', done => {
+    const data = {
+      email: 'johnDoe@email.com',
+      password: '123456',
+    };
+
+    request(app)
+      .post('/api/login')
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .end((err, res) => {
+        expect(res.body.error).toMatch(
+          'login failed, email and password not match'
+        );
+        done(err);
+      });
+  });
+  test('test with invalid request password', done => {
+    const data = {
+      email: 'nisha.sharma@phe.gov.uk',
+      password: '123456563322',
+    };
+
+    request(app)
+      .post('/api/login')
+      .send(data)
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .end((err, res) => {
+        expect(res.body.error).toMatch(
+          'login failed, email and password not match'
+        );
+        done(err);
+      });
+  });
+});
