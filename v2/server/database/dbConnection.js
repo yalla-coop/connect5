a12e5dc49c3d5c14dcf8905815762bd37ec99a9e
marwajomaa <marwajomaa4@gmail.com>
2019-05-16 16:15:53 +0300

    remove console.log

--- v2/server/database/dbConnection.js
+++ v2/server/database/dbConnection.js
@@ -7,11 +7,9 @@ let mongoURI = process.env.MONGO_URI;
 if (process.env.NODE_ENV === 'test') {
   // change mongoURI to testing database URI
   mongoURI = process.env.MONGOURI_TEST;
-  console.log('0000000000', mongoURI);
 } else if (process.env.NODE_ENV !== 'production') {
   // change mongoURI to testing database URI
   mongoURI = process.env.MONGOURI_DEV;
-  console.log('0000000000', mongoURI);
 }
 
 // create DB connection
