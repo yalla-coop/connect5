90971c7d36c857d4f77feb0ff7036b168ce1b15a
ramy <ramyshurafa@hotmail.com>
2019-05-19 11:55:09 +0300

    feat: trainer signup back-end

---
+++ v2/server/controllers/users/trainer/signUp.js
@@ -0,0 +1,46 @@
+const boom = require('boom');
+const jwt = require('jsonwebtoken');
+const { tokenMaxAge } = require('./../../../constants');
+
+const {
+  createNewTrainer,
+} = require('./../../../database/queries/users/trainer');
+
+module.exports = async (req, res, next) => {
+  const { name, email, password, localLeadId, region } = req.body;
+  if (name && email && password && localLeadId && region) {
+    return createNewTrainer({
+      name,
+      email,
+      password,
+      localLead: localLeadId,
+      region,
+      role: 'localLead',
+    })
+      .then(trainer => {
+        const trainerInfo = {
+          id: trainer._id,
+          name: trainer.name,
+          role: trainer.role,
+          organization: trainer.organization,
+          region: trainer.region,
+          email: trainer.email,
+        };
+
+        // create token for 25 day
+        const token = jwt.sign({ id: trainer._id }, process.env.SECRET, {
+          expiresIn: tokenMaxAge.string,
+        });
+
+        res.cookie('token', token, {
+          maxAge: tokenMaxAge.number,
+          httpOnly: true,
+        });
+        return res.json(trainerInfo);
+      })
+      .catch(err => {
+        next(boom.badImplementation());
+      });
+  }
+  return next(boom.badRequest('Some arguments are missed'));
+};
--- v2/server/router/users.js
+++ v2/server/router/users.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const { getTrianerReachData } = require('../controllers/users/trainer');
 
 const getLocalLeads = require('../controllers/users/getLocalLeads');
+const signUpTrainer = require('../controllers/users/trainer/signUp');
 
 router.get('/trainer/info', getTrianerReachData);
+router.post('/trainers', signUpTrainer);
 
 router.get('/local-leads', getLocalLeads);
 
