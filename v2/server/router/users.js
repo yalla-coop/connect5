ae01c99122d9689837bab3247bd2c174c9637c4e
ramy <ramyshurafa@hotmail.com>
2019-05-20 02:28:04 +0300

    test: checkUniqueEmail API

--- v2/server/router/users.js
+++ v2/server/router/users.js
@@ -9,6 +9,8 @@ const signUpTrainer = require('../controllers/users/trainer/signUp');
 const checkUniqueEmail = require('./../controllers/users/checkUniqueEmail');
 const { getDashboardStats } = require('../controllers/users/all');
 
+// check eamil route if the route doen't contain email query
+// then it will skip with next()
 router.get('/users', checkUniqueEmail);
 
 router.get('/trainer/info', getTrianerReachData);
