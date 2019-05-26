9cf673dd90f540fabe727cc8f49c7c6c7d104c1a
marwajomaa <marwajomaa4@gmail.com>
2019-05-26 17:00:10 +0300

    cop the link of the survey and pop up msg for success

--- v2/client/src/components/pages/SessionDetails/SessionSurveys/SurveyContent.js
+++ v2/client/src/components/pages/SessionDetails/SessionSurveys/SurveyContent.js
@@ -12,6 +12,7 @@ import {
 } from './SessionSurveys.Style';
 
 class SurveyContent extends Component {
+  // Fire Info pop up
   onInfoClick = () => {
     Swal.fire({
       title: 'Info',
@@ -22,18 +23,55 @@ class SurveyContent extends Component {
     });
   };
 
+  // Copy the link of the survey and fire pop up for success
+  onCopyClick = () => {
+    const copyText = document.getElementById('link');
+    let range;
+    let selection;
+    if (document.body.createTextRange) {
+      range = document.body.createTextRange();
+      range.moveToElementText(copyText);
+      range.select();
+    } else if (window.getSelection) {
+      selection = window.getSelection();
+      range = document.createRange();
+      range.selectNodeContents(copyText);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+
+    try {
+      document.execCommand('copy');
+      Swal.fire({
+        title: 'Success',
+        text: 'Link copied!',
+        type: 'success',
+        timer: 2000,
+        confirmButtonText: 'Ok',
+      });
+    } catch (err) {
+      Swal.fire({
+        title: 'Error',
+        text: 'Unable to cop the Link',
+        type: 'error',
+        timer: 2000,
+        confirmButtonText: 'Ok',
+      });
+    }
+  };
+
   render() {
-    const { onInfoClick } = this;
+    const { onInfoClick, onCopyClick } = this;
     return (
       <SurveyContentWrapper>
         <SurveyLinkType>Survey 1 Link </SurveyLinkType>
         <SurveyLinkInfo onClick={onInfoClick}>
           <Icon type="info-circle" />
         </SurveyLinkInfo>
-        <SurveyLink to="https://ant.design/components/icons">
+        <SurveyLink to="https://ant.design/components/icons" id="link">
           https://ant.design/components/icons
         </SurveyLink>
-        <CopyLink>
+        <CopyLink onClick={onCopyClick}>
           <Icon type="copy" />
           <IconName>Copy Link</IconName>
         </CopyLink>
