1579fbc8919414bf8b617cdcdb9596fdace3d53f
ramy <ramyshurafa@hotmail.com>
2019-06-12 01:02:09 +0300

    session results controller

--- v2/server/controllers/survey/getSurveyResponses.js
+++ v2/server/controllers/survey/getSurveyResponses.js
@@ -4,12 +4,23 @@ const {
   getAllResponsesOnSurvey,
 } = require('../../database/queries/surveys/surveyResponses');
 
+const { getAttendeesNumber } = require('../../database/queries/surveys');
+
 module.exports = async (req, res, next) => {
   const { sessionId, surveyType } = req.params;
+  const promisesArray = [
+    getAllResponsesOnSurvey(sessionId, surveyType),
+    getAttendeesNumber(sessionId, surveyType),
+  ];
 
-  return getAllResponsesOnSurvey(sessionId, surveyType)
-    .then(responses => {
-      res.json({ responses });
+  return Promise.all(promisesArray)
+    .then(result => {
+      const [responses, attendeesNumber] = result;
+      res.json({
+        responses,
+        attendeesNumber: attendeesNumber.numberOfAttendees,
+        responsesNumber: responses.length + 1,
+      });
     })
     .catch(err => next(boom.badImplementation()));
 };
