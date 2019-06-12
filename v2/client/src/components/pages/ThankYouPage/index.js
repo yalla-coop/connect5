ae4dd668deab4fc0d2fae2a4a0cb69e4d2ccdb57
marwajomaa <marwajomaa4@gmail.com>
2019-06-12 16:06:47 +0300

    style enhancement

--- v2/client/src/components/pages/ThankYouPage/index.js
+++ v2/client/src/components/pages/ThankYouPage/index.js
@@ -19,8 +19,10 @@ const ThankYouPage = () => {
           width="200px"
           height="50px"
           style={{
-            padding: '10px 14px',
+            padding: '12px 14px',
+            borderRadius: '20px',
             textTransform: 'none',
+            fontWeight: '400',
           }}
         />
       </StyledLink>
