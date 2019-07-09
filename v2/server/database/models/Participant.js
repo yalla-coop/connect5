0b5bced3ae720f8dd71eabcb4b2dfcda447ecfe2
ramy <ramyshurafa@hotmail.com>
2019-07-09 15:10:15 +0300

    create participants schema

---
+++ v2/server/database/models/Participant.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const { Schema, model } = mongoose;
+
+const { questionConstants } = require('./../DBConstants');
+
+const ParticipantSchema = new Schema(
+  {
+    PIN: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    age: {
+      type: String,
+      enum: questionConstants.ages,
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+    },
+    ethinc: String,
+    region: String,
+    postcode: String,
+    Session1Date: Date,
+    occupation: String,
+    workforce: String,
+  },
+  { timestamps: true }
+);
+
+const Participant = model('participants', ParticipantSchema);
+module.exports = Participant;
