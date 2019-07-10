81b7b1767e4e36fb462437807452eb610f9291c4
ramy <ramyshurafa@hotmail.com>
2019-07-10 12:31:59 +0300

    fix failed test

--- v2/server/controllers/survey/getSurveyQs.js
+++ v2/server/controllers/survey/getSurveyQs.js
@@ -2,18 +2,20 @@ const boom = require('boom');
 const surveyQs = require('../../database/queries/surveys/surveyQuestions');
 
 module.exports = (req, res, next) => {
-  // get the dynamic id from req.params. This should be a string with the surveytype as the first part. The rest of the string is the unique sessionId
+  // get the dynamic id from req.params. This should be a string with the surveytype as the first part. The rest of the string is the unique shortId
   const { id } = req.params;
   const idParts = id.split('&');
 
   // assign the surveyId and sessionId to their own variables
   const surveyType = idParts[0];
-  const sessionId = idParts[1];
+  const shortId = idParts[1];
 
   // initiate queries function to get the right questions from the database, the session details and then send these to the client
-  surveyQs(surveyType, sessionId)
+  surveyQs(surveyType, shortId)
     .then(surveyDetails => {
       res.status(200).json(surveyDetails);
     })
-    .catch(next(boom.badImplementation()));
+    .catch(err => {
+      next(boom.badImplementation());
+    });
 };
