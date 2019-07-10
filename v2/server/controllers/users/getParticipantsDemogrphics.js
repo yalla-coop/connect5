9956457f3eeb9ccb5be327300c55bece863d6951
ramy <ramyshurafa@hotmail.com>
2019-07-10 17:29:30 +0300

    get participants demographics data back-end Relates #216

---
+++ v2/server/controllers/users/getParticipantsDemogrphics.js
@@ -0,0 +1,15 @@
+const boom = require('boom');
+
+const getParticipantsDemogrphics = require('../../database/queries/users/getParticipantsDemogrphics');
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const data = await getParticipantsDemogrphics(id);
+
+    return res.json(data);
+  } catch (err) {
+    return next(boom.badImplementation());
+  }
+};
---
+++ v2/server/database/queries/users/getParticipantsDemogrphics.js
@@ -0,0 +1,129 @@
+const Participant = require('./../../models/Participant');
+const { regions, questionConstants } = require('./../../DBConstants');
+
+const getParticipantsDemogrphics = () =>
+  Participant.aggregate([
+    {
+      $match: {
+        region: { $exists: true },
+        gender: { $exists: true },
+        age: { $exists: true },
+        ethnic: { $exists: true },
+      },
+    },
+    {
+      $facet: {
+        total: [
+          {
+            $group: {
+              _id: null,
+              total: {
+                $sum: 1,
+              },
+            },
+          },
+        ],
+        region: [
+          {
+            $match: {
+              region: { $in: regions },
+            },
+          },
+          {
+            $group: {
+              _id: '$region',
+              sum: {
+                $sum: 1,
+              },
+            },
+          },
+          {
+            $sort: {
+              sum: -1,
+            },
+          },
+        ],
+        gender: [
+          {
+            $match: {
+              gender: { $in: ['male', 'female'] },
+            },
+          },
+          {
+            $group: {
+              _id: '$gender',
+              sum: {
+                $sum: 1,
+              },
+            },
+          },
+          {
+            $sort: {
+              sum: -1,
+            },
+          },
+        ],
+        age: [
+          {
+            $match: {
+              age: { $in: questionConstants.ages },
+            },
+          },
+          {
+            $group: {
+              _id: '$age',
+              sum: {
+                $sum: 1,
+              },
+            },
+          },
+          {
+            $sort: {
+              sum: -1,
+            },
+          },
+        ],
+        ethnic: [
+          {
+            $match: {
+              ethnic: { $in: questionConstants.ethnics },
+            },
+          },
+          {
+            $group: {
+              _id: '$ethnic',
+              sum: {
+                $sum: 1,
+              },
+            },
+          },
+          {
+            $sort: {
+              sum: -1,
+            },
+          },
+        ],
+        occupation: [
+          {
+            $group: {
+              _id: '$occupation',
+              sum: {
+                $sum: 1,
+              },
+              totalCount: { $sum: 1 },
+            },
+          },
+          {
+            $sort: {
+              sum: -1,
+            },
+          },
+          {
+            $limit: 7,
+          },
+        ],
+      },
+    },
+  ]);
+
+module.exports = getParticipantsDemogrphics;
