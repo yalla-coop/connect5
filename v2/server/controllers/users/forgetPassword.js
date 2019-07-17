28a628aba0a2b4b7be31f779cded70533dfebe31
marwajomaa <marwajomaa4@gmail.com>
2019-07-17 23:06:30 +0300

    sent user reset password link by email

--- v2/server/controllers/users/forgetPassword.js
+++ v2/server/controllers/users/forgetPassword.js
@@ -1,28 +1,46 @@
 const boom = require('boom');
-const { getUserByEmail } = require('./../../database/queries/users');
+const crypto = require('crypto');
+const { resetTokenMaxAge } = require('./../../constants');
+const {
+  getUserByEmail,
+  updateUserById,
+} = require('./../../database/queries/users');
+const resetPasswordMailing = require('../../helpers/emails/resetPasswordByEmail');
 
 module.exports = (req, res, next) => {
   const { email } = req.query;
-  if (!email) {
-    return next();
-  }
 
-  return getUserByEmail(email)
-    .then(user => {
-      if (!user) {
-        return next(
-          boom.unauthorized('login failed, email and password not match')
-        );
-      }
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+    }
+    const token = buffer.toString('hex');
 
-      const userData = {
-        id: user._id,
-        role: user.role,
-        name: user.name,
-        email: user.email,
-      };
-    })
-    .catch(err => {
-      boom.badImplementation();
-    });
+    return getUserByEmail(email)
+      .then(user => {
+        if (!user) {
+          return next(boom.unauthorized('No account with that email found'));
+        }
+        const { _id } = user;
+
+        const data = {
+          resetToken: {
+            value: token,
+            expiresIn: Date.now() + resetTokenMaxAge,
+          },
+        };
+        updateUserById(_id, data)
+          // send the token via email
+
+          .then(() => resetPasswordMailing(email, token, user.name))
+          .then(() => {
+            //  send success message
+            res.json({ success: true });
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        next(boom.badImplementation());
+      });
+  });
 };
