fa3405ebe0d24bbdebd3e0c9158686c331caaa1f
marwajomaa <marwajomaa4@gmail.com>
2019-08-01 12:13:22 +0300

    create about us page

--- v2/client/src/components/pages/LandingPage/LandingPage.style.js
+++ v2/client/src/components/pages/LandingPage/LandingPage.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-import { colors, breakpointsMax } from '../../../theme';
+import { colors, breakpointsMax, borders } from '../../../theme';
 
 export const Wrapper = styled.div`
   display: flex;
@@ -83,4 +83,140 @@ export const ButtonDiv = styled.div`
     color: ${colors.white};
     background-color: ${colors.primary};
   }
+`;
+
+// Style for about us page
+
+export const AboutUsWrapper = styled.div`
+  display; flex;
+  flex-direction: column;
+  padding: 2rem .3rem;
+  @media (min-width: 768px) {
+    width: 60%;
+    margin: 0 auto
+  }
+`;
+
+export const Summury = styled.div``;
+
+export const Heading = styled.h2`
+  font-size: 1.5rem;
+  font-weight: 400;
+  line-height: 1.2;
+  margin-bottom: 22px;
+  text-align: center;
+  position: relative;
+  color: ${colors.primary};
+  @media (min-width: 768px) {
+    font-size: 2rem;
+    font-weight: 500;
+  }
+
+  &::after {
+    content: ' ';
+    position: absolute;
+    border-bottom: ${borders.heading};
+    bottom: -6px;
+    left: 72px;
+    width: 45%;
+    @media (min-width: 768px) {
+      width: 25%;
+      left: 37%;
+    }
+  }
+`;
+
+export const H3 = styled.h3`
+  font-size: 1rem;
+  font-weight: 400;
+  text-align: center;
+  padding: 1.5rem 0;
+`;
+
+export const H4 = styled.h4`
+  font-weight: 300;
+  text-align: center;
+  padding-top: 0.9rem;
+`;
+
+export const P = styled.p`
+  line-height: 1.4;
+  text-align: center;
+  margin: 0px 10px;
+  color: ${colors.blackSecondary};
+`;
+
+export const Mission = styled.div``;
+
+export const About = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: ${colors.transLightGray};
+  margin-top: 1.5rem;
+  @media (min-width: 768px) {
+    margin-top: 2rem;
+  }
+`;
+
+export const Question = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  margin: 0 auto;
+  margin-bottom: 20px;
+  color: #6a6a6a;
+  padding: 0 5px;
+  overflow: hidden;
+`;
+
+export const Ul = styled.li`
+  padding-left: 10px;
+  margin: 20px 0;
+  list-style: none;
+`;
+
+export const Li = styled.li`
+  margin-bottom: 25px;
+  display: list-item;
+  font-size: 1rem;
+  font-weight: 300;
+  text-align: left;
+`;
+
+export const Blockquote = styled.div`
+  display: block;
+  grid-area: blockquote;
+  background-color: ${colors.backgroundWashOut};
+  color: ${colors.red};
+  border: none;
+  position: relative;
+  margin: 0 0 20px;
+  padding: 20px;
+`;
+
+export const Block = styled.blockquote`
+  margin-top: 30px;
+  width: 100%;
+  &:before {
+    content: '\2706';
+    font-size: 30px;
+  }
+  &:after {
+    content: '\2192';
+  }
+`;
+
+export const Quote = styled.p`
+  margin-bottom: 20px;
+  margin: 40 0 20px;
+  line-height: 1.4;
+  font-weight: 400;
+  font-size: 1rem;
+`;
+
+export const Speaker = styled.p`
+  color: #3e3327;
+  font-size: 16px;
+  margin: 0 0 30px;
+  line-height: 1.4;
 `;
--- v2/client/src/components/pages/LandingPage/index.js
+++ v2/client/src/components/pages/LandingPage/index.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Drawer } from 'antd';
+import AboutUs from './AboutUs';
 import {
   Wrapper,
   LogoContainer,
@@ -21,51 +23,78 @@ import Connect5Logo from '../../../assets/connect-5-white.png';
 
 import Button from '../../common/Button';
 
-export default function LandingPage() {
-  return (
-    <Wrapper>
-      <LogoContainer>
-        {' '}
-        <Logo src={Connect5Logo} alt="logo" />
-      </LogoContainer>
-      <DescriptionContainer>
-        <Headline>Welcome to the Connect5</Headline>
-        <Paragraph>
-          Welcome to the App of the mental health training programme Connect 5
-        </Paragraph>
-      </DescriptionContainer>
-      <ButtonContainer>
-        <ButtonLink to={`${LOGIN_URL}`}>
-          <ButtonDiv>
-            <Button
-              label="I'm a Trainer or Local Lead"
-              width="100%"
-              height="100%"
-              type="primary"
-            />
-          </ButtonDiv>
-        </ButtonLink>
-        <ButtonLink to={`${PARTICIPANT_LOGIN}`}>
-          <ButtonDiv>
-            <Button
-              label="I'm a Course Participant"
-              width="100%"
-              height="100%"
-              type="primary"
-            />
-          </ButtonDiv>
-        </ButtonLink>
-        <ButtonLink to="/">
-          <ButtonDiv>
-            <Button
-              label="I want to find out more"
-              width="100%"
-              height="100%"
-              type="primary"
-            />
-          </ButtonDiv>
-        </ButtonLink>
-      </ButtonContainer>
-    </Wrapper>
-  );
+class LandingPage extends Component {
+  state = { visible: false };
+
+  onClose = () => {
+    this.setState({ visible: false });
+  };
+
+  DrawerOpen = () => {
+    this.setState({ visible: true });
+  };
+
+  render() {
+    const { visible } = this.state;
+    const { onClose, DrawerOpen } = this;
+    return (
+      <Wrapper>
+        <LogoContainer>
+          {' '}
+          <Logo src={Connect5Logo} alt="logo" />
+        </LogoContainer>
+        <DescriptionContainer>
+          <Headline>Welcome to the Connect5</Headline>
+          <Paragraph>
+            Welcome to the App of the mental health training programme Connect 5
+          </Paragraph>
+        </DescriptionContainer>
+        <ButtonContainer>
+          <ButtonLink to={`${LOGIN_URL}`}>
+            <ButtonDiv>
+              <Button
+                label="I'm a Trainer or Local Lead"
+                width="100%"
+                height="100%"
+                type="primary"
+              />
+            </ButtonDiv>
+          </ButtonLink>
+          <ButtonLink to={`${PARTICIPANT_LOGIN}`}>
+            <ButtonDiv>
+              <Button
+                label="I'm a Course Participant"
+                width="100%"
+                height="100%"
+                type="primary"
+              />
+            </ButtonDiv>
+          </ButtonLink>
+          <ButtonLink to="/">
+            <ButtonDiv>
+              <Button
+                label="I want to find out more"
+                width="100%"
+                height="100%"
+                type="primary"
+                onClick={DrawerOpen}
+              />
+              <Drawer
+                placement="left"
+                width="100%"
+                height="100%"
+                onClose={onClose}
+                visible={visible}
+                closable
+              >
+                <AboutUs />
+              </Drawer>
+            </ButtonDiv>
+          </ButtonLink>
+        </ButtonContainer>
+      </Wrapper>
+    );
+  }
 }
+
+export default LandingPage;
