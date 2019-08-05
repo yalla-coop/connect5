e4d23215f0fa78b374f8accd5590ffeb73382be6
marwajomaa <marwajomaa4@gmail.com>
2019-08-05 14:50:59 +0300

    add header to create session

--- v2/client/src/components/pages/CreateSession/index.js
+++ v2/client/src/components/pages/CreateSession/index.js
@@ -16,6 +16,7 @@ import {
 import moment from 'moment';
 import { connect } from 'react-redux';
 import Button from '../../common/Button';
+import Header from '../../common/Header';
 import { fetchAllTrainers } from '../../../actions/trainerAction';
 import {
   fetchLocalLeads,
@@ -320,7 +321,7 @@ class CreateSession extends Component {
 
     return (
       <CreateSessionWrapper>
-        <Heading>Create New Session</Heading>
+        <Header type="section" label="Create New Session" />
         <Form onSubmit={onFormSubmit}>
           <InputDiv>
             <DatePicker
