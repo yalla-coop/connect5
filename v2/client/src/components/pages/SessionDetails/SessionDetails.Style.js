6c0fbd88b28821f9805cd701ffc4f30ec0a74e9b
marwajomaa <marwajomaa4@gmail.com>
2019-08-09 15:25:39 +0300

    fix responsive for manage session page

--- v2/client/src/components/pages/SessionDetails/SessionDetails.Style.js
+++ v2/client/src/components/pages/SessionDetails/SessionDetails.Style.js
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { borders, colors } from '../../../theme';
+import { borders, colors, breakpoints } from '../../../theme';
 
 export const SessionDetailsWrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+  max-width: 600px
   margin: 0 auto;
-  @media (min-width: 768px) {
-    width: 50%;
+  padding: 5rem 0.3rem;
+  @media ${breakpoints.mobileL} {
+    width: 90%;
+    padding: 6rem 0.5rem;
   }
-  padding-bottom: 5rem;
-  padding-top: 5rem;
 `;
 
 export const SessionTopDetailsWrapper = styled.div`
