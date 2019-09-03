201c5a822ed7b664a20199a26c18ee92715cd61b
marwajomaa <marwajomaa4@gmail.com>
2019-09-03 11:40:22 +0300

    add pop up in reach and Behavioural

--- v2/client/src/components/common/Reach/index.js
+++ v2/client/src/components/common/Reach/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Table } from 'antd';
 import styled from 'styled-components';
+import AntdModal from '../AntdModal';
 
 const sessionsColumns = [
   {
@@ -45,15 +46,24 @@ const surveysColumns = [
 ];
 
 const Head = styled.h3`
-  font-size: 24px;
+  font-size: 20px;
   color: #4f4f4f;
   padding: 2rem 0 0 2rem;
-  color: rgba(0, 0, 0, 0.85);
+  color: rgba(0, 0, 0, 0.8);
 `;
+
+const content =
+  'This section provides an overview of sessions and related surveys collected via the app. I you wish to export the results click on "Export as CSV" and it will export and download raw data (CSV format)';
 
 const Reach = ({ data }) => {
   return (
     <div>
+      <AntdModal
+        title="About this section"
+        content={content}
+        btnStyle={{ margin: '1.5rem' }}
+        style={{ top: '20' }}
+      />
       <Head>Sessions</Head>
       <Table
         columns={sessionsColumns}
