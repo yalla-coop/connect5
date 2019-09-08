a5efb33d73b921716affff3c9600dc07f1a1de88
marwajomaa <marwajomaa4@gmail.com>
2019-09-08 12:12:20 +0300

    add officialLocalLead to sign up front and backend

--- v2/client/src/components/pages/SignUp/index.js
+++ v2/client/src/components/pages/SignUp/index.js
@@ -43,6 +43,8 @@ class SignUp extends Component {
     confirmDirty: false,
     givenPermission: true,
     role: 'trainer',
+    checkLocalLead: false,
+    officialLocalLead: false,
   };
 
   componentDidMount() {
@@ -79,22 +81,31 @@ class SignUp extends Component {
 
   onChangeCheckbox = e => {
     let role = '';
+    const { officialLocalLead } = this.state;
     if (e.target.checked) {
       role = 'localLead';
     } else {
       role = 'trainer';
     }
-    this.setState({ role });
+    this.setState({
+      role,
+      checkLocalLead: e.target.checked,
+      officialLocalLead: officialLocalLead && e.target.checked,
+    });
+  };
+
+  onChangeCheckboxOfficialLocalLead = e => {
+    this.setState({ officialLocalLead: e.target.checked });
   };
 
   handleSubmit = e => {
-    const { role, givenPermission } = this.state;
+    const { role, givenPermission, officialLocalLead } = this.state;
 
     const { form, signUpTrainer: signUpTrainerActionCreator } = this.props;
     e.preventDefault();
     form.validateFieldsAndScroll((err, values) => {
       if (!err && givenPermission) {
-        signUpTrainerActionCreator({ role, ...values });
+        signUpTrainerActionCreator({ role, officialLocalLead, ...values });
       }
     });
   };
@@ -153,7 +164,7 @@ class SignUp extends Component {
   };
 
   render() {
-    const { role, givenPermission } = this.state;
+    const { role, givenPermission, checkLocalLead } = this.state;
     const {
       form: { getFieldDecorator },
       localLeads,
@@ -163,12 +174,18 @@ class SignUp extends Component {
       loading,
     } = this.props;
 
-    const { onChangeCheckbox, onChangeCheckboxPermission } = this;
+    const {
+      onChangeCheckbox,
+      onChangeCheckboxPermission,
+      onChangeCheckboxOfficialLocalLead,
+    } = this;
 
     if (isAuthenticated) {
       history.push('/dashboard');
       return null;
     }
+
+    console.log(this.state);
 
     return (
       <>
@@ -298,6 +315,21 @@ class SignUp extends Component {
                   trainers
                 </span>
               </Checkbox>
+
+              {checkLocalLead && (
+                <Checkbox
+                  onChange={onChangeCheckboxOfficialLocalLead}
+                  style={{
+                    textAlign: 'left',
+                    marginBottom: '24px',
+                  }}
+                >
+                  <span style={{ fontSize: '.9rem' }}>
+                    I{"'"}m an official Connect 5 Local Lead
+                  </span>
+                </Checkbox>
+              )}
+
               {role !== 'localLead' && (
                 <Item style={{ margin: '0 auto 20' }}>
                   {getFieldDecorator('localLead', {
