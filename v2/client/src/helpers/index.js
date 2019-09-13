d3ca365c7971f24d3b145c89895734625cd6d80b
Simon Dupree <dupreenator@gmail.com>
2019-09-13 14:26:25 +0200

    merge conflicts resolved 4

--- v2/client/src/helpers/index.js
+++ v2/client/src/helpers/index.js
@@ -80,3 +80,9 @@ export const getAllSurveyLinks = (sessionType, shortId) => {
 export const getSessionSurveys = sessionType => {
   return surveysTypes[sessionType];
 };
+
+export const validPostcode = postcode => {
+  postcode.replace(/\s/g, '');
+  const regex = /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/;
+  return regex.test(postcode);
+};
