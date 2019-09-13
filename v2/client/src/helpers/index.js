8c6144d2734cf8cd551c8facb727ecf7845cc5cb
Simon Dupree <dupreenator@gmail.com>
2019-09-13 14:14:10 +0200

    merge conflicts resolved 2

--- v2/client/src/helpers/index.js
+++ v2/client/src/helpers/index.js
@@ -3,7 +3,6 @@ export const uppercaseSurvey = surveyType =>
     .split('-')
     .map(item => item[0].toLocaleUpperCase() + item.slice(1))
     .join(' ');
-<<<<<<< HEAD
 
 const surveysTypes = {
   1: ['pre-day-1', 'post-day-1'],
@@ -81,12 +80,3 @@ export const getAllSurveyLinks = (sessionType, shortId) => {
 export const getSessionSurveys = sessionType => {
   return surveysTypes[sessionType];
 };
-||||||| merged common ancestors
-=======
-
-export const validPostcode = postcode => {
-  postcode.replace(/\s/g, '');
-  const regex = /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})/;
-  return regex.test(postcode);
-};
->>>>>>> develop
