11ad25dc59ca978952ff70ee2224f04699ae6290
ramy <ramyshurafa@hotmail.com>
2019-09-16 11:43:25 +0300

    refactor get sessions query

--- v2/server/database/queries/users/getLeadSessions.js
+++ v2/server/database/queries/users/getLeadSessions.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const Session = require('../../models/Session');
-const User = require('../../models/User');
 
 // sum of trainer sessions and atendees grouped by type
 module.exports.getTrianerSessions = trainerId => {
@@ -13,50 +12,5 @@ module.exports.getTrianerSessions = trainerId => {
 };
 
 module.exports.getLocalLeadSessions = localLeadId => {
-  return User.aggregate([
-    {
-      $match: { _id: mongoose.Types.ObjectId(localLeadId) },
-    },
-    {
-      $unwind: '$trainersGroup',
-    },
-    {
-      $lookup: {
-        from: 'sessions',
-        localField: 'trainersGroup',
-        foreignField: 'trainers',
-        as: 'sessions',
-      },
-    },
-    {
-      $match: { sessions: { $exists: true, $ne: [] } },
-    },
-    {
-      $group: {
-        _id: null,
-        sessions: { $addToSet: '$sessions' },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        sessions: {
-          $reduce: {
-            input: '$sessions',
-            initialValue: [],
-            in: {
-              $concatArrays: ['$$this', '$$value'],
-            },
-          },
-        },
-      },
-    },
-    { $unwind: '$sessions' },
-    {
-      $group: {
-        _id: '$_id',
-        sessions: { $addToSet: '$sessions' },
-      },
-    },
-  ]);
+  return Session.find({ canAccessResults: localLeadId });
 };
