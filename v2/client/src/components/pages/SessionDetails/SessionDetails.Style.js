add9287c9dcd211d66a8009e73c7a493055017e0
ramy <ramyshurafa@hotmail.com>
2019-10-02 15:43:35 +0300

    fix travis

--- v2/client/src/components/pages/SessionDetails/SessionDetails.Style.js
+++ v2/client/src/components/pages/SessionDetails/SessionDetails.Style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { borders, colors, breakpoints } from '../../../theme';
+import { borders, colors } from '../../../theme';
 
 export const SessionDetailsWrapper = styled.div`
   display: flex;
