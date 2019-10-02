97851b7cf07edc75c12128feacef29be6579f82b
ramy <ramyshurafa@hotmail.com>
2019-10-02 16:59:04 +0300

    fix the range of stars values => 0 - 10

--- v2/client/src/components/pages/survey/index.js
+++ v2/client/src/components/pages/survey/index.js
@@ -359,7 +359,8 @@ class Survey extends Component {
   handleStarChange = (answer, question) => {
     const { formState } = this.state;
     // remove 1 from the answer so it's 0 to 5 not 1 to 6
-    const fixedAnswer = { answer: answer - 1, question };
+    // then multuply the answer by 2 so it's 0 to 10 not 0 to 5 :)
+    const fixedAnswer = { answer: (answer - 1) * 2, question };
     this.setState(
       { formState: { ...formState, [question]: fixedAnswer } },
       () => {
