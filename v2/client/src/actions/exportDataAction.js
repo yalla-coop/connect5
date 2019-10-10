1b8e20d5a7e6663483c15b09a2a2d3f9d4ce517f
ramy <ramyshurafa@hotmail.com>
2019-10-10 12:36:32 +0300

    refactor the export function front-end Relates #610

--- v2/client/src/actions/exportDataAction.js
+++ v2/client/src/actions/exportDataAction.js
@@ -3,9 +3,12 @@ import { message } from 'antd';
 
 import { EXPORT_DATA_SUCCESS } from '../constants/actionTypes';
 
-export const exportDataAction = (filter, trainerIDs) => async dispatch => {
+export const exportDataAction = (filters, cb) => async dispatch => {
   axios
-    .post(`/api/export-csv`, { filter, trainerIDs })
-    .then(({ data }) => dispatch({ type: EXPORT_DATA_SUCCESS, payload: data }))
+    .post(`/api/export-csv`, { filters })
+    .then(({ data }) => {
+      return dispatch({ type: EXPORT_DATA_SUCCESS, payload: data });
+    })
+    .then(cb)
     .catch(() => message.error('Error! something went wrong'));
 };
--- v2/client/src/components/common/ExportButton/index.js
+++ v2/client/src/components/common/ExportButton/index.js
@@ -3,109 +3,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CSVLink } from 'react-csv';
-import { message } from 'antd';
-
-import axios from 'axios';
 
 import Button from '../Button';
 
 import { exportDataAction } from '../../../actions/exportDataAction';
 
 class ExportButton extends Component {
-  state = {
-    dataToExport: [],
-    filter: false,
-    trainerIDs: null,
-  };
-
-  componentDidMount() {
-    const { role, selectedUser, userId } = this.props;
-    // check if there's a selectedUserRole
-
-    // if trainer then just filter for that trainer
-    if (role === 'trainer') {
-      this.setState({ filter: true, trainerIDs: [userId] });
-    }
-
-    if (role === 'localLead') {
-      // if locallead but wanting to export results for specific trainer
-      if (selectedUser && selectedUser.role === 'trainer') {
-        this.setState({ filter: true, trainerIDs: [selectedUser._id] });
-      } else {
-        // get all the trainer ids for that local lead
-        this.getTrainerIDs(userId);
-      }
-    }
-
-    if (role === 'admin') {
-      // if admin and looking at the results for a local lead
-      if (selectedUser && selectedUser.role === 'localLead') {
-        // get list of that localLeads trainers
-        this.getTrainerIDs(selectedUser._id);
-      } else if (selectedUser && selectedUser.role === 'trainer') {
-        // get the results for that specific trainer
-        this.setState({ filter: true, trainerIDs: [selectedUser._id] });
-      }
-    }
-  }
-
-  getTrainerIDs = localLeadID => {
-    axios
-      .post('/api/users/locallead-trainers', { localLeadID })
-      .then(res => {
-        let trainerIDs = [];
-
-        if (res.data.trainerList && res.data.trainerList.length > 0) {
-          trainerIDs = res.data.trainerList.map(trainer => trainer._id);
-        }
-
-        this.setState({ filter: true, trainerIDs });
-      })
-      .catch(err => console.log(err));
-  };
+  clickBtn = React.createRef();
 
   fetchExportData = () => {
     // get the view level from the component
-    const { filter, trainerIDs } = this.state;
+    const { exportData, filters } = this.props;
 
-    const searchData = { filter, trainerIDs };
-
-    this.setState({ dataToExport: [] });
-
-    axios
-      .post('/api/export-csv', { searchData })
-      .then(({ data }) => {
-        this.setState(
-          {
-            dataToExport: data,
-          },
-          () => {
-            const link = document.getElementById('csvLink');
-            link.click();
-          }
-        );
-      })
-      .catch(err => {
-        const error =
-          err.response && err.response.data && err.response.data.error;
-        message.error(error || 'Something went wrong');
-      });
+    const cb = this.clickBtn.current.link.click;
+    exportData(filters, cb);
   };
 
   render() {
-    const { dataToExport } = this.state;
+    const { csvData } = this.props;
     return (
       <>
         <CSVLink
-          data={dataToExport}
+          data={csvData}
           asyncOnClick
           id="csvLink"
           style={{ display: 'none' }}
+          ref={this.clickBtn}
         />
         <Button
           label="Export CSV"
           type="primary"
-          ref={this.clickBtn}
           onClick={this.fetchExportData}
           width="200px"
           margin="1rem auto"
