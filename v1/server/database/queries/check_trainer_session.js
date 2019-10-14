841f7c5de22c9b6efe16dd235acb966b6b435b94
ramy <ramyshurafa@hotmail.com>
2019-10-14 12:43:03 +0300

    delete v1 files and put v2 on root Relates #655

--- v1/server/database/queries/check_trainer_session.js
+++
@@ -1,7 +0,0 @@
-const Session = require("./../models/Session");
-
-const getAllAnswers = async (sessionId, trainerId) => {
-  const sessions = await Session.findOne({ _id: sessionId, trainer: trainerId });
-  return !!sessions;
-};
-module.exports = getAllAnswers;
