07ac052e169a4949d3dad8674f56bb6a6af79703
ramy <ramyshurafa@hotmail.com>
2019-10-14 10:12:47 +0300

    remove commented code and return back files deleted accedentally

--- v2/client/src/components/common/EditEmail/index.js
+++ v2/client/src/components/common/EditEmail/index.js
@@ -24,7 +24,10 @@ import * as Yup from 'yup';
 
 import { getPostSurveyLink, getPreSurveyLink } from '../../../helpers';
 
-import { MY_SESSIONS_URL, MY_GROUP_SESSIONS_URL } from '../../../constants/navigationRoutes';
+import {
+  MY_SESSIONS_URL,
+  MY_GROUP_SESSIONS_URL,
+} from '../../../constants/navigationRoutes';
 import Header from '../Header';
 import InfoPopUp from '../InfoPopup';
 
@@ -238,7 +241,6 @@ class EditEmail extends Component {
 
   done = () => {
     const { backCallback, role } = this.props;
-    console.log("ROLE", role);
 
     Modal.success({
       title: 'Done!',
@@ -452,7 +454,7 @@ class EditEmail extends Component {
       endTime,
       backCallback,
       isSchedule,
-      role
+      role,
     } = this.props;
 
     let title = '';
@@ -796,7 +798,7 @@ class EditEmail extends Component {
                 loading={loading}
               />
               <BackLink onClick={() => history.push(MY_SESSIONS_URL)}>
-              Invite people later and go back to session overview
+                Invite people later and go back to session overview
               </BackLink>
               {error}
               {isEditView && (
@@ -811,7 +813,8 @@ class EditEmail extends Component {
                   onClick={() => {
                     if (typeof backCallback === 'function')
                       return backCallback();
-                    if (role !== 'trainer') return history.push(MY_GROUP_SESSIONS_URL);
+                    if (role !== 'trainer')
+                      return history.push(MY_GROUP_SESSIONS_URL);
                     return history.push(MY_SESSIONS_URL);
                   }}
                 >
--- v2/server/controllers/sessionDetails/updateAttendeesList.js
+++ v2/server/controllers/sessionDetails/updateAttendeesList.js
@@ -24,7 +24,6 @@ module.exports = (req, res, next) => {
     .then(async () => {
       // get session Details
       const sessionDetails = await getSessionById(sessionId);
-      console.log(sessionDetails, '---------------------------');
       const { date, type } = sessionDetails;
       const sessionDate = date.toString();
       const confirmedEmails = participantsEmails.map(
@@ -42,13 +41,6 @@ module.exports = (req, res, next) => {
         surveyTypes: [ThreeMonthSurvey, SixMonthSurvey],
       });
 
-      const sessionDetails2 = await getSessionById(sessionId);
-      console.log(sessionDetails2, '++++++++++++++++++++++++++++');
-
-      // await removeEmailBySurveyType({
-      //   sessionId,
-      //   surveyType: SixMonthSurvey,
-      // });
       // add new ones
       await scheduleNewEmail({
         sessionId,
@@ -63,13 +55,9 @@ module.exports = (req, res, next) => {
         date: getScheduleDates(sessionDate, 6),
       });
 
-      const sessionDetails3 = await getSessionById(sessionId);
-      console.log(sessionDetails3, '============================');
-
       return res.json({});
     })
     .catch(err => {
-      console.log(err);
       next(boom.badImplementation());
     });
 };
