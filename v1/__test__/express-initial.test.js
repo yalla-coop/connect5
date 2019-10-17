5659398f3b20c86ad8e1cb6526a45d7ad63db586
ramy <ramyshurafa@hotmail.com>
2019-10-17 12:49:45 +0300

    delete v1

--- v1/__test__/express-initial.test.js
+++
@@ -1,3 +0,0 @@
-test("basic maths to show jest works: 11 + 22 === 33", () => {
-  expect(11 + 22).toBe(33);
-});
--- v1/client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./Components/App";
-
-import "./index.css";
-import "./normalize.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
